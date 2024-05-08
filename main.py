fe104c38d3293ba3b2d6e955f35d36ff4e8e6fa8
patri <patrik.rusinak@gmail.com>
2024-05-08 16:22:42 +0200

    kod na upravu

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ from torchmetrics.detection.mean_ap import MeanAveragePrecision
 
 import numpy as np
 
-#parameters_path = "C:/Users/patri/PycharmProjects/semHSU/model_parameters.pth"
-# parameters_path = "C:/Users/patri/PycharmProjects/semHSU/model_parameters_mse.pth"
-parameters_path = "C:/D/Desktop/School/4.Rocnik/Hlboke_strojove_ucenie/Semestralka/semHSU/model_parameters.pth"
+parameters_path = "C:/Users/patri/PycharmProjects/semHSU/model_parameters.pth"
+# parameters_path = "C:/D/Desktop/School/4.Rocnik/Hlboke_strojove_ucenie/Semestralka/semHSU/model_parameters.pth"
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
@@ -88,7 +87,7 @@ val_loader = DataLoader(val_set, batch_size=32, shuffle=True, collate_fn=collate
 
 # print(train_set[0][0].shape) #zistenie shape dat
 
-model = models.detection.ssd300_vgg16(pretrained=False)  # for training
+model = models.detection.ssd300_vgg16(num_classes=2, pretrained=False)  # for training
 
 # testujeme model upravu
 # model = models.detection.ssd300_vgg16() #pretreined for testing
@@ -99,108 +98,136 @@ model = model.to(device)
 
 # getting the optimizer and loss_function
 
-def get_essentials():
-    #loss_fun = nn.L1Loss()
-    loss_fun = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-    return loss_fun, optimizer
-
-
-def train_batch(img, label, model, loss_fun, optimizer):
-    images = torch.stack(img)
-    # labels = torch.stack(label)
-    model.train()
-    pred_points = model(images.to(device), label) #vlozim do modelu cely batch
-    # print(startX, startY, endX, endY)
-    # print(pred_points[0]) #testovaci print tu by asi chcelo si vypisat tie body ze co to vlastne je alebo jak
-    # loss_val = loss_fun(pred_points.cpu(), labels) #vystup pred_points bude mat druhy rozmer 1000 tak musim enastavit aj labels
-    # loss_val.backward()
-    # optimizer.step()
-    # optimizer.zero_grad()
-    return pred_points['bbox_regression'].item()
-
-@torch.no_grad()
-def val_batch(img, label, model, loss_fun, optimizer):
-    images = torch.stack(img)
-    model.eval()
-    pred_points = model(images.to(device))
-    # labels = torch.stack(label)
-    # loss_val = loss_fun(pred_points.cpu(), labels)
-    metric = MeanAveragePrecision(iou_type="bbox")
-
-    # Update metric with predictions and respective ground truth
-    metric.update(pred_points, label)
-
-    # Compute the results
-    precision = metric.compute()
-
-    return precision['map']
-
-
-epochs = 2
-loss_fun, optimizer = get_essentials()
-
-#Trening a validacia
-# # training and validation loops
+# def get_essentials():
+#     #loss_fun = nn.L1Loss()
+#     loss_fun = nn.MSELoss()
+#     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+#     return loss_fun, optimizer
 #
-train_epoch, val_epoch = [], []
-for epoch in range(epochs):
-    print('Epoch: ', epoch)
-    train_batch_losses, val_batch_losses = [], []
-    print('Train loader')
-    for img, labels in train_loader:
-        train_batch_loss = train_batch(img, labels, model, loss_fun, optimizer)
-        train_batch_losses.append(train_batch_loss)
-    print('Validation loader')
-    for img, labels in val_loader:
-        val_batch_loss = val_batch(img, labels, model, loss_fun, optimizer)
-        val_batch_losses.append(val_batch_loss)
-    train_epoch.append(np.mean(train_batch_losses))
-    val_epoch.append(np.mean(val_batch_losses))
-    print('Train: ' + np.mean(train_batch_losses))
-    print('Validation: ' + np.mean(val_batch_losses)) #tu uz by mal byt precision
-
-#save model parameters
-torch.save(model.state_dict(), f=parameters_path)
-
-plt.plot(range(epochs), train_epoch, label="train_loss")
-plt.plot(range(epochs), val_epoch, label="val_loss")
-plt.legend()
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
-plt.title("Training Wider Face model")
-plt.show()
+#
+# def train_batch(img, label, model, loss_fun, optimizer):
+#     images = torch.stack(img)
+#     model.train()
+#     optimizer.zero_grad()
+#     loss_dict = model(images.to(device), label) #vlozim do modelu cely batch
+#
+#     losses = sum(loss for loss in loss_dict.values())
+#     loss_value = losses.item()
+#
+#     losses.backward()
+#     optimizer.step()
+#     return loss_value
+#
+# @torch.no_grad()
+# def val_batch(img, label, model, loss_fun, optimizer):
+#     images = torch.stack(img)
+#     model.eval()
+#     target = []
+#     preds = []
+#
+#     outputs = model(images.to(device))
+#
+#     # For mAP calculation using Torchmetrics.
+#     #####################################
+#     for m in range(len(images)):
+#         true_dict = dict()
+#         preds_dict = dict()
+#         true_dict['boxes'] = label[m]['boxes'].detach().cpu()
+#         true_dict['labels'] = label[m]['labels'].detach().cpu()
+#         preds_dict['boxes'] = outputs[m]['boxes'].detach().cpu()
+#         preds_dict['scores'] = outputs[m]['scores'].detach().cpu()
+#         preds_dict['labels'] = outputs[m]['labels'].detach().cpu()
+#         preds.append(preds_dict)
+#         target.append(true_dict)
+#     #####################################
+#
+#     metric = MeanAveragePrecision()
+#     metric.update(preds, target)
+#
+#     # Compute the results
+#     precision = metric.compute()
+#
+#     return precision['map']
+#
+#
+# epochs = 30
+# loss_fun, optimizer = get_essentials()
+#
+# #Trening a validacia
+# # # training and validation loops
+#
+# train_epoch, val_epoch = [], []
+# for epoch in range(epochs):
+#     print('Epoch: ', epoch)
+#     train_batch_losses, val_batch_losses = [], []
+#     print('Train loader')
+#     for img, labels in train_loader:
+#         train_batch_loss = train_batch(img, labels, model, loss_fun, optimizer)
+#         train_batch_losses.append(train_batch_loss)
+#     print('Validation loader')
+#     for img, labels in val_loader:
+#         val_batch_loss = val_batch(img, labels, model, loss_fun, optimizer)
+#         val_batch_losses.append(val_batch_loss)
+#     train_epoch.append(np.mean(train_batch_losses))
+#     val_epoch.append(np.mean(val_batch_losses))
+#     #print('Train: ' + np.mean(train_batch_losses))
+#     #print('Validation: ' + np.mean(val_batch_losses)) #tu uz by mal byt precision
+#
+# #save model parameters
+# torch.save(model.state_dict(), f=parameters_path)
+#
+# plt.plot(range(epochs), train_epoch, label="train_loss")
+# plt.plot(range(epochs), val_epoch, label="val_loss")
+# plt.legend()
+# plt.xlabel("Epochs")
+# plt.ylabel("Loss")
+# plt.title("Training Wider Face model")
+# plt.show()
 
 #testovanie modelu
-# model.load_state_dict(torch.load(parameters_path, map_location=device))
-# for img_batch, dummy in test_loader:
-#     model.eval()
-#     with torch.no_grad():
-#         for img_out in img_batch:
-#             img = torch.unsqueeze(img_out, dim=0)
-#             outputs = model(img.to(device))
-#             img_out = torch.permute(img_out, (1, 2, 0))
-#             # get score for all the predicted objects
-#             pred_scores = outputs[0]['scores'].detach().cpu().numpy()
-#             # get all the predicted bounding boxes
-#             pred_bboxes = outputs[0]['boxes'].detach().cpu().numpy()
-#             # get boxes above the threshold score
-#             boxes = pred_bboxes[pred_scores >= detection_threshold].astype(np.int32)
-#             plt.subplot(121)
-#             plt.imshow(img_out)
-#             plt.title(" Actual Image ")
-#             fig = plt.gcf()
-#             ax = fig.add_subplot(122)
-#             plt.imshow(img_out)
-#             patches = []
-#             for i in range(boxes.shape[0]): #vypis bounding boxov
-#                 x = boxes[i][0]
-#                 y = boxes[i][1]
-#                 w = boxes[i][2] - x
-#                 h = boxes[i][3] - y
-#                 rect1 = plt.Rectangle((x, y), width=w, height=h, fill=False, edgecolor='red',
-#                                              linewidth=1, facecolor='none')
-#                 ax.add_patch(rect1)
-#             plt.title(" Actual Image with keyponts")
-#             plt.show()
+model.load_state_dict(torch.load(parameters_path, map_location=device))
+for img_batch, label in val_loader:
+    model.eval()
+    with torch.no_grad():
+        for i in range(len(img_batch)):
+            img = torch.unsqueeze(img_batch[i], dim=0)
+            model.eval()
+            pred_points = model(img.to(device))
+            # labels = torch.stack(label)
+            # loss_val = loss_fun(pred_points.cpu(), labels)
+            metric = MeanAveragePrecision(iou_type="bbox")
+
+            # Update metric with predictions and respective ground truth
+            testik = list([label[i]])
+            metric.update(pred_points, testik)
+
+            # Compute the results
+            precision = metric.compute()
+
+            # img = torch.unsqueeze(img_out, dim=0)
+            # outputs = model(img.to(device))
+            img_out = torch.permute(img.squeeze(), (1, 2, 0))
+            # # get score for all the predicted objects
+            # pred_scores = outputs[0]['scores'].detach().cpu().numpy()
+            # # get all the predicted bounding boxes
+            # pred_bboxes = outputs[0]['boxes'].detach().cpu().numpy()
+            # # get boxes above the threshold score
+            # boxes = pred_bboxes[pred_scores >= detection_threshold].astype(np.int32)
+            plt.subplot(121)
+            plt.imshow(img_out)
+            plt.title(" Actual Image ")
+            fig = plt.gcf()
+            ax = fig.add_subplot(122)
+            plt.imshow(img_out)
+            patches = []
+            for i in range(pred_points[0]['boxes'].shape[0]): #vypis bounding boxov
+                x = pred_points[0]['boxes'][i][0].cpu()
+                y = pred_points[0]['boxes'][i][1].cpu()
+                w = pred_points[0]['boxes'][i][2].cpu() - x
+                h = pred_points[0]['boxes'][i][3].cpu() - y
+                rect1 = plt.Rectangle((x, y), width=w, height=h, fill=False, edgecolor='red',
+                                             linewidth=1, facecolor='none')
+                ax.add_patch(rect1)
+            plt.title(" Actual Image with keyponts")
+            plt.show()
 
